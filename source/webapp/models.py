291398462c2d6e68192fb87dd596c725b788b48b
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-19 12:21:05 +0600

    Создана модель Опроса и модель Варианта ответа

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.TextField(max_length=50, blank=False, null=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    choice_text = models.TextField(max_length=50, blank=False, null=False, verbose_name='Вариант ответа')
+    poll = models.ForeignKey('webapp.Poll', related_name='choices', on_delete=models.CASCADE, verbose_name='Опрос')
