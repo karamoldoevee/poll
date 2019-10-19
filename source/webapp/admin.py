aeb44ea7aba2a173783bb271123b0ba9fd0180b8
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-19 12:32:57 +0600

    Модель зарегистрированна в админ-панели

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+class ChoiceAdmin(admin.TabularInline):
+    model = Choice
+    fields = ['question']
+    extra = 0
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_at']
+    list_filter = ['question']
+    list_display_links = ['pk', 'question']
+    search_fields = ['question']
+    exclude = []
+    readonly_fields = ['created_at']
+    inlines = [ChoiceAdmin]
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
