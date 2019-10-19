ac278a11795876f0919f1d5a162e575902d245e5
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-19 13:11:50 +0600

    Добавлена Форма опроса и Выбора ответа

---
+++ source/webapp/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from webapp.models import Poll, Choice
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        exclude = ['created_at']
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        exclude = []
+
+class PollChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
