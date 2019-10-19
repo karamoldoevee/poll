21fecc222f7e9ab8e725321657cc50d2e5ffa2a1
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-19 14:51:35 +0600

    Добавлен UpdateView для выбора ответа

--- source/webapp/views/choice_views.py
+++ source/webapp/views/choice_views.py
@@ -31,4 +31,15 @@ class ChoiceForPollCreateView(CreateView):
         poll_pk = self.kwargs.get('pk')
         return get_object_or_404(Poll, pk=poll_pk)
 
+class ChoiceUpdateView(UpdateView):
+    model = Choice
+    template_name = 'comment/update.html'
+    form_class = PollChoiceForm
+    context_object_name = 'choice'
 
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse('poll_view', kwargs={'pk': self.object.poll.pk})
