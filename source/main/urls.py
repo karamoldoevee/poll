dec9cdb8176da42b7c77bcaa52a692233afa9226
Karamoldoev Emir <karamoldoevee@gmail.com>
2019-10-19 13:46:04 +0600

    Добавлены urls для CRUD опроса

--- source/main/urls.py
+++ source/main/urls.py
@@ -1,21 +1,12 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from webapp.views.poll_views import IndexView, PollView, PollCreateView, PollUpdateView, PollDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:pk>/', PollView.as_view(), name='poll_view'),
+    path('article/add/', PollCreateView.as_view(), name='poll_add'),
+    path('article/<int:pk>/edit/', PollUpdateView.as_view(), name='poll_update'),
+    path('article/<int:pk>/delete/', PollDeleteView.as_view(), name='poll_delete'),
 ]
